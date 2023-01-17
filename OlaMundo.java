0a678f091d93680fb1bf03be141b723ef0a00880
Diego Roque <diegojoroque@gmail.com>
2023-01-17 18:43:10 -0300

    Add files via upload

---
+++ OlaMundo.java
@@ -0,0 +1,14 @@
+/*
+Exercício Aula 05
+
+1. Criar um arquivo chamado OlaMundo.java.
+2. Implementar um programa Java que escreva na tela "Olá Mundo".
+3. Compilar e executar o programa.
+
+link da correção do exercício: https://youtu.be/3rZNefsMmKs
+*/
+
+class OlaMundo {
+
+    
+}
