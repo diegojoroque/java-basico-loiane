1e8e494c70e2731b31898bcad696cd8cef45513f
Diego Roque <diegojoroque@gmail.com>
2023-01-18 20:41:51 -0300

    Aula-06

---
+++ aula-06/Argumentos.java
@@ -0,0 +1,8 @@
+class Argumentos {
+    public static void main (String[] args){
+
+        System.out.println("Você digitou " + args[0]);
+    }
+}
+
+    //Ao insirer um "argumento" após o comando de execução do programa, por ex: java Argumentos Aula-06, o terminal retornará esse argumento. Deve ser digitado sem espaço ou entre "" para inserir espaços.
---
+++ aula-06/Argumentos2.java
@@ -0,0 +1,16 @@
+/*
+Exercício Aula 06
+
+1. Criar um arquivo chamado Argumentos2.java.
+2. Implementar um programa Java que escreva na tela "Olá Mundo! " + o argumento.
+3. Compilar e executar o programa.
+
+link da correção do exercício: https://www.youtube.com/watch?v=Ebvl0BlnD-U
+*/
+
+class Argumentos2 {
+    public static void main (String [] args) {
+
+        System.out.println("Olá Mundo! " + args[0]);
+    }
+}
